c524a4228414958da4ace89f061cbf5c7a22c433
khalil <khalil.rmila@esprit.tn>
2024-05-14 16:44:47 +0100

    "Add reclamation response functionality through AJAX, update template with modal for adding responses"
    This commit adds the ability to add a response to a reclamation through an AJAX request in the ReponseController, and

--- citiezenHub_webapp/src/Controller/ReponseController.php
+++ citiezenHub_webapp/src/Controller/ReponseController.php
@@ -2,17 +2,41 @@
 
 namespace App\Controller;
 
+use App\Entity\Reponse;
+use App\Entity\Reclamation;
+use App\Repository\ReclamationRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ReponseController extends AbstractController
 {
-    #[Route('/reponse', name: 'app_reponse')]
-    public function index(): Response
+    #[Route('/reclamation/{id}/response', name: 'app_add_response', methods: ['POST'])]
+    public function addResponse(Request $request, EntityManagerInterface $entityManager, ReclamationRepository $reclamationRepository): JsonResponse
     {
-        return $this->render('reponse/index.html.twig', [
-            'controller_name' => 'ReponseController',
-        ]);
+        $data = json_decode($request->getContent(), true);
+        $reclamationId = $data['reclamationId'] ?? null;
+        $responseText = $data['responseText'] ?? null;
+
+        if (!$reclamationId || !$responseText) {
+            return new JsonResponse(['success' => false, 'message' => 'Invalid data']);
+        }
+
+        $reclamation = $reclamationRepository->find($reclamationId);
+
+        if (!$reclamation) {
+            return new JsonResponse(['success' => false, 'message' => 'Reclamation not found']);
+        }
+
+        $reponse = new Reponse();
+        $reponse->setDescription($responseText);
+        $reponse->setReclamation($reclamation);
+
+        $entityManager->persist($reponse);
+        $entityManager->flush();
+
+        return new JsonResponse(['success' => true]);
     }
 }
