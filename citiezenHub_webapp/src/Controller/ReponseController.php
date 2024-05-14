a3cf33097179347ab59d6e8f6f4019e98e91d72f
khalil <khalil.rmila@esprit.tn>
2024-05-14 17:04:12 +0100

    "Updated ReponseController and added Future Prediction section in reponse index view"
    This commit includes the following changes:
    
    1. Modified ReponseController by removing the `addResponse` method and its related usages

--- citiezenHub_webapp/src/Controller/ReponseController.php
+++ citiezenHub_webapp/src/Controller/ReponseController.php
@@ -2,41 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\Reponse;
-use App\Entity\Reclamation;
-use App\Repository\ReclamationRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ReponseController extends AbstractController
 {
-    #[Route('/reclamation/{id}/response', name: 'app_add_response', methods: ['POST'])]
-    public function addResponse(Request $request, EntityManagerInterface $entityManager, ReclamationRepository $reclamationRepository): JsonResponse
+    #[Route('/reponse', name: 'app_reponse')]
+    public function index(): Response
     {
-        $data = json_decode($request->getContent(), true);
-        $reclamationId = $data['reclamationId'] ?? null;
-        $responseText = $data['responseText'] ?? null;
-
-        if (!$reclamationId || !$responseText) {
-            return new JsonResponse(['success' => false, 'message' => 'Invalid data']);
-        }
-
-        $reclamation = $reclamationRepository->find($reclamationId);
-
-        if (!$reclamation) {
-            return new JsonResponse(['success' => false, 'message' => 'Reclamation not found']);
-        }
-
-        $reponse = new Reponse();
-        $reponse->setDescription($responseText);
-        $reponse->setReclamation($reclamation);
-
-        $entityManager->persist($reponse);
-        $entityManager->flush();
-
-        return new JsonResponse(['success' => true]);
+        return $this->render('reponse/index.html.twig', [
+            'controller_name' => 'ReponseController',
+        ]);
     }
 }
